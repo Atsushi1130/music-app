adf453480de4e8b79dc7cb83959e51afabdeafe6
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-07-24 18:19:12 +0900

    heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module RailsMusicApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
+    config.assets.js_compressor = :uglifier
+
 
     # Configuration for the application, engines, and railties goes here.
     #
